6dd273be496d93696135ce9e2405a8c802fb48ae
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2026-01-27 10:04:44 +0530

    feat(mongoose): schema models for all data collections

---
+++ src/models/conversation.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+import type { ConversationInterface } from "#/interfaces/index.js";
+
+const ConversationSchema = new Schema<ConversationInterface>({
+  participants: [
+    {
+      type: Schema.Types.ObjectId,
+      refPath: "models",
+    },
+  ],
+  models: {
+    type: String,
+    enum: ["User", "Group"],
+    required: true,
+  },
+  interaction: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Conversation = model<ConversationInterface>("Conversation", ConversationSchema);
+
+export default Conversation;
---
+++ src/models/index.ts
@@ -0,0 +1,4 @@
+export { default as Conversation } from "./conversation.js";
+export { default as Group } from "./group.js";
+export { default as Message } from "./message.js";
+export { default as User } from "./user.js";
