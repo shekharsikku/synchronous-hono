a5ca133367e977bb8e52ac98c9bbb1b5ddebbd37
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2026-01-27 10:10:36 +0530

    refactor(schema): define response data and zod schema

---
+++ src/utils/response.ts
@@ -0,0 +1,33 @@
+import type { Context } from "hono";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
+
+export class HttpError extends Error {
+  public code: ContentfulStatusCode;
+
+  constructor(code: ContentfulStatusCode, message: string, stack: string = "") {
+    super(message);
+    this.code = code;
+    this.message = message;
+    this.stack = stack;
+  }
+}
+
+type TypeResponse<T = any, E = any> = {
+  code?: ContentfulStatusCode;
+  success: boolean;
+  message: string;
+  data?: T;
+  error?: E;
+};
+
+export const ErrorResponse = <E = any>(ctx: Context, code: ContentfulStatusCode, message: string, error?: E) => {
+  const response: TypeResponse<null, E> = { success: false, message };
+  if (error !== undefined) response.error = error;
+  return ctx.json<TypeResponse<null, E>>(response, code);
+};
+
+export const SuccessResponse = <T = any>(ctx: Context, code: ContentfulStatusCode, message: string, data?: T) => {
+  const response: TypeResponse<T, null> = { success: true, message };
+  if (data !== undefined) response.data = data;
+  return ctx.json<TypeResponse<T, null>>(response, code);
+};
